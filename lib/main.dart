d002b597548962b9fece31b0fb13108644ed5c32
Miguel Velasco <migue16velasco@gmail.com>
2026-02-26 06:45:42 -0400

    feat: add animation

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,12 @@ class MyHttpOverrides extends HttpOverrides {
 }
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   HttpOverrides.global = MyHttpOverrides();
   cameras = await availableCameras();
+
   runApp(const MyApp());
 }
 
@@ -52,48 +55,23 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _fadeController;
-  late Animation<double> _fadeAnim;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
 
-    _fadeController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-    );
+    // Remueve splash nativo apenas Flutter esté listo
+    FlutterNativeSplash.remove();
 
-    _fadeAnim = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _fadeController, curve: Curves.easeIn));
-
-    _fadeController.forward();
-
-    // Navegar después de que termina la animación Lottie
-    Future.delayed(const Duration(milliseconds: 3500), () {
+    // Espera duración de la animación
+    Future.delayed(const Duration(seconds: 3), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
-          PageRouteBuilder(
-            pageBuilder: (_, __, ___) => const SessionCompany(),
-            transitionsBuilder: (_, animation, __, child) {
-              return FadeTransition(opacity: animation, child: child);
-            },
-            transitionDuration: const Duration(milliseconds: 600),
-          ),
+          MaterialPageRoute(builder: (_) => const SessionCompany()),
         );
       }
     });
-  }
-
-  @override
-  void dispose() {
-    _fadeController.dispose();
-    super.dispose();
   }
 
   @override
@@ -103,15 +81,12 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnim,
-          child: Lottie.asset(
-            'assets/animations/face_scan.json',
-            width: size.width * 0.65,
-            height: size.width * 0.65,
-            fit: BoxFit.contain,
-            repeat: true,
-          ),
+        child: Lottie.asset(
+          'assets/animations/face_scan.json',
+          width: size.width * 0.7,
+          height: size.width * 0.7,
+          fit: BoxFit.contain,
+          repeat: true,
         ),
       ),
     );
